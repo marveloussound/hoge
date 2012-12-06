c92d68b87792cdf654cc5f9cade99abf33ebcf95
AA AA <marveloussound@mbp.local>
2012-12-06 15:45:42 +0900

    Initial Commit

---
+++ sample1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  sample1
+//
+//  Created by AA AA on 12/12/06.
+//  Copyright (c) 2012年 AA AA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
