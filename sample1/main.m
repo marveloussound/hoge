9b911b7e472465ff4cbf07dc811ca7a4efa35e8e
marveloussound <marveloussound@gmail.com>
2012-12-10 18:32:00 +0900

    Init

--- sample1/main.m
+++ sample1/main.m
@@ -8,14 +8,61 @@
 
 #import <Foundation/Foundation.h>
 
+
+double calcWeight(double height){
+  const double BMI = 22;
+  double stdWeight;
+  
+  stdWeight = pow((height /100),2) * BMI;
+  return stdWeight;
+  
+  
+}
+
+
 int main(int argc, const char * argv[])
 {
-
+  
+  
   @autoreleasepool {
+    
+    int maxNum = 10;
+    int sum = 0;
+    int test = 1;
+    NSMutableString *str1 = [[NSMutableString alloc] initWithCapacity:10];
+    
+    NSMutableString *str2 = [NSMutableString stringWithCapacity:10];
+    NSString *str3 = [NSString new];
+    str3 = @"xxz";
+    
+    [str1 appendString:@"01234567890"];
+    [str2 appendString:@"aa"];
+    
+      //[str1 appendString:@"cc"];
+    
+    str3 = [str3 stringByAppendingString:@"bb"];
+    str3 = [str3 stringByAppendingString:@"dd"];
+    
+    
+    str3 = [str1 substringWithRange:NSMakeRange(2,4)];
+    
+    
+    
+    NSLog(@"%@",str3);
+    
+      if([str1 isEqualToString:str2]){
+          //if(str1 == str2){
+      NSLog(@"%@",@"ok!!!");
+    }
+    
+    for(int i=0;i<=maxNum;i++){
       
-      // insert code here...
-      NSLog(@"Hello, World!");
-      
+      sum += i;
+
+    }
+    
+    NSLog(@"1 to %d =>%d",maxNum,sum);
+    
   }
     return 0;
 }
